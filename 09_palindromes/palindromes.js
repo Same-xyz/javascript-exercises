0a347ad83e5850345366eacbaf1d787bc96a1ff0
Same-xyz <samakubor@gmail.com>
2025-02-01 01:13:38 +0100

    solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
 const palindromes = function (words) {
-    
+    //My Imperative Solution
+    // let reversed = '';
+    // const cleanedWords = words.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    // for (let i = cleanedWords.length - 1; i >= 0; i--) {
+    //     reversed += cleanedWords[i]
+    // }
+    // return cleanedWords === reversed;
+    //Actual Functional Solution
+    //We convert the string to lower case to make the comparison case-insensitive
+    //We remove all non-alphanumeric characters to make the comparison ignore punctuation and spaces
+    //We split the cleaned string into an array of individual characters
+    //We reverse the array of characters
+    //We join the reversed array back into a single string
+    //We compare the reversed string to the cleaned original string
+    cleanWords = words.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    return words
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9]/g, '')
+        .split('')
+        .reverse()
+        .join('') === cleanWords;
 };
 
 // Do not edit below this line
